b166d6979d498013306b2feb4ce1c208019a843f
Chribtoper <xcstolfax@gmail.com>
2018-09-10 17:49:05 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def dictionary
+hash = {
+"hello" => 'hi',
+'to' => '2',
+'two' => '2', 
+'too' => '2', 
+'for' => '2',
+'four' => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@", 
+"and" => "&"
+}
+end
+
+def word_substituter
+  
+end
