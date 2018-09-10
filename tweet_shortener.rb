b7eebbe9badf46cb3cc3f858ecc7890beddd2999
Chribtoper <xcstolfax@gmail.com>
2018-09-10 19:24:19 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,7 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
-def selective_tweet_shortener(tweets)
+def selective_tweet_shortener(tweet)
     if tweet.size > 140
       word_substituter(tweet)
     else
